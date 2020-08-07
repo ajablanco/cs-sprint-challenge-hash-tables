c439434c1087f618fab035b357c8f8ff35fd4567
Aja Blanco <aja@Ajas-MBP.attlocal.net>
2020-08-07 09:21:05 -0700

    ex4.py done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,22 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    
+    hash_tbl = {}
+    result = []
+
+
+    for num in a:
+        abs_val = abs(num)
+        if abs_val in hash_tbl:
+            hash_tbl[abs_val] += 1
+            result.append(abs_val)
+        else:
+            hash_tbl[abs_val] = 1
 
     return result
+
+print(has_negatives([1, -1, 2, -2, 3, 4, 5, -5]))
 
 
 if __name__ == "__main__":
